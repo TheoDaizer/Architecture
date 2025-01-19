ea2904a6adea750027a1c81876f2e4d7191c986e
Theodore <theo.daizer@gmail.com>
2025-01-19 23:35:09 +0500

    Fix discriminant calculations and add nan validation

--- module_1_4/quadratic_equation_solver.py
+++ module_1_4/quadratic_equation_solver.py
@@ -1,4 +1,5 @@
-from math import sqrt
+from math import sqrt, isclose, isfinite
+
 
 class QuadraticEquationSolver:
     """Class for work with quadratic equation"""
@@ -15,13 +16,16 @@ class QuadraticEquationSolver:
         :param c: coefficient c
         :return: list of roots
         """
-        if abs(a) < cls.epsilon:
-            raise ValueError("Coefficient a should not be zero")
+        if not all(isfinite(coef) for coef in (a, b, c)):
+            raise ValueError('Coefficients must not be NaN or Infinity')
 
-        d = 2 * b - 4 * a * c
+        if abs(a) < cls.epsilon:
+            raise ValueError("Coefficient 'a' should not be zero")
+
+        d = cls.discriminant(a, b, c)
 
         roots = []
-        if abs(d) < cls.epsilon:
+        if isclose(d, 0, rel_tol=0, abs_tol=cls.epsilon):
             roots.append(-b / (2 * a))
         elif d > 0:
             sqrt_d = sqrt(d)
@@ -30,3 +34,15 @@ class QuadraticEquationSolver:
             roots.sort()
 
         return roots
+
+    @classmethod
+    def discriminant(cls, a: float, b: float, c: float) -> float:
+        """
+        Return discriminant (b^2 - 4 * a * c)
+
+        :param a: coefficient a
+        :param b: coefficient b
+        :param c: coefficient c
+        :return: discriminant
+        """
+        return b**2 - 4 * a * c
--- module_1_4/test_quadratic_equation_solver.py
+++ module_1_4/test_quadratic_equation_solver.py
@@ -1,4 +1,5 @@
 import pytest
+from math import isclose, nan, inf
 from .quadratic_equation_solver import QuadraticEquationSolver
 
 
@@ -11,7 +12,7 @@ class TestUM:
     def check_result(self, reference, result):
 
         assert len(result) == len(reference)
-        assert all(abs(result[i] - reference[i]) < self.epsilon for i in range(len(reference)))
+        assert all(isclose(result[i], reference[i], rel_tol=0, abs_tol=self.epsilon) for i in range(len(reference)))
 
     def test_no_roots(self):
         a, b, c = 1.0, 0.0 , 1.0
@@ -31,14 +32,18 @@ class TestUM:
         a, b, c = 1.0, 2.0, 1.0
         reference = (-1.0,)
         result = self.solver.solve(a, b, c)
+        d = self.solver.discriminant(a, b, c)
 
+        assert d == 0
         self.check_result(reference, result)
 
-    def test_one_non_int_root(self):
-        a, b, c = 0.55, 3.3, 3.0
-        reference = (-3.0,)
+    def test_one_root_for_non_zero_d(self):
+        a, b, c = 1.0, 2.0, 1.0 + self.epsilon / 8
+        reference = (-1.0,)
         result = self.solver.solve(a, b, c)
+        d = self.solver.discriminant(a, b, c)
 
+        assert d != 0
         self.check_result(reference, result)
 
     def test_a_equal_zero(self):
@@ -48,5 +53,18 @@ class TestUM:
 
     def test_a_smaller_then_epsilon(self):
         a, b, c = self.solver.epsilon / 2, 0 , 0
+        with pytest.raises(ValueError):
+            self.solver.solve(a, b, c)
+
+    def test_nan_inf_coefficients(self):
+        a, b, c = nan, nan, nan
+        with pytest.raises(ValueError):
+            self.solver.solve(a, b, c)
+
+        a, b, c = inf, inf, inf
+        with pytest.raises(ValueError):
+            self.solver.solve(a, b, c)
+
+        a, b, c = -inf, -inf, -inf
         with pytest.raises(ValueError):
             self.solver.solve(a, b, c)
